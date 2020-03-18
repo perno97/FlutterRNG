c4954c87b30476aa9e657a0bc9b058db549c3535
perno97 <fabio.perini97@gmail.com>
2020-03-18 23:45:40 +0100

    Added app icon

--- lib/main.dart
+++ lib/main.dart
@@ -13,50 +13,56 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
       home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _random_number;
-  int _from;
-  int _to;
+  final fromController = TextEditingController();
+  final toController = TextEditingController();
+  String _randomNumber = "-";
+  int _from = -1;
+  int _to = -1;
+  bool ready = true;
 
-  void _generateRandomNumber() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _random_number = _from + Random.secure().nextInt(_to +1);
-    });
+  Future<void> _generateRandomNumber() async {
+    if(ready) {
+      ready = false;
+      setState(() {
+        _randomNumber = "-";
+      });
+      await Future.delayed(const Duration(seconds: 1), () {
+        setState(() {
+          if(fromController.text.length != 0)
+            _from = int.parse(fromController.text);
+          if(toController.text.length != 0)
+            _to = int.parse(toController.text);
+          int minimum = min(_from,_to);
+          int maximum = max(_from,_to);
+          int number = minimum + Random.secure().nextInt(maximum - minimum + 1);
+          if(number >= 0)
+            _randomNumber = number.toString();
+          ready = true;
+        });
+      });
+    }
+  }
+
+  @override
+  void dispose(){
+    fromController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       body: Column(
         mainAxisSize: MainAxisSize.max,
@@ -66,8 +72,8 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text(
-                  '$_random_number',
-                  style: Theme.of(context).textTheme.display1,
+                  "$_randomNumber",
+                  style: Theme.of(context).textTheme.display4.apply(fontSizeFactor: 1.5, fontWeightDelta: 5),
                 )
               ],
             )
@@ -75,20 +81,35 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
                     Expanded(
                       child: Padding(
                         padding: EdgeInsets.all(15.0),
                         child: TextField(
+                          textAlign: TextAlign.center,
+                          controller: fromController,
+                          style: TextStyle(fontWeight: FontWeight.bold),
                           decoration: new InputDecoration(
-                              labelText: "Da"
+                            labelText: "Da",
+                            labelStyle: TextStyle(
+                              color: Colors.white
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white)
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white)
+                            )
                           ),
                           keyboardType: TextInputType.number,
                           inputFormatters: <TextInputFormatter>[
                             WhitelistingTextInputFormatter.digitsOnly,
-                            LengthLimitingTextInputFormatter(5)
+                            LengthLimitingTextInputFormatter(4)
                           ],
                         )
                       )
@@ -97,13 +118,25 @@ class _MyHomePageState extends State<MyHomePage> {
                         child: Padding(
                           padding: EdgeInsets.all(15.0),
                           child: TextField(
+                            textAlign: TextAlign.center,
+                            controller: toController,
+                            style: TextStyle(fontWeight: FontWeight.bold),
                             decoration: new InputDecoration(
-                                labelText: "A"
+                              labelText: "A",
+                              labelStyle: TextStyle(
+                                  color: Colors.white
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white)
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white)
+                              )
                             ),
                             keyboardType: TextInputType.number,
                             inputFormatters: <TextInputFormatter>[
                               WhitelistingTextInputFormatter.digitsOnly,
-                              LengthLimitingTextInputFormatter(5)
+                              LengthLimitingTextInputFormatter(4)
                             ],
                           )
                         )
